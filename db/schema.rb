32b0d67f04dd9ae41b7e4cbb55e00079f86b636f
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-01-28 21:48:16 -0200

    DATABASE CHANGE: Adiciona constraints 'NOT NULL' em colunas da tabela order_statuses.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_28_233329) do
+ActiveRecord::Schema.define(version: 2019_01_28_234443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,7 +93,7 @@ ActiveRecord::Schema.define(version: 2019_01_28_233329) do
   end
 
   create_table "order_statuses", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
