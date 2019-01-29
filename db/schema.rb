dd79ec87965394f63fa0a3db00c775c18d5f3883
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-01-28 23:23:06 -0200

    DATABASE CHANGE: Adiciona constraints 'NOT NULL' em colunas da tabela users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_011003) do
+ActiveRecord::Schema.define(version: 2019_01_29_011752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -147,11 +147,11 @@ ActiveRecord::Schema.define(version: 2019_01_29_011003) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.integer "age"
-    t.string "cpf"
-    t.integer "role"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.integer "age", null: false
+    t.string "cpf", null: false
+    t.integer "role", null: false
     t.bigint "company_id"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
