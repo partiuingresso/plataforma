ed8b6b053645439cc2775e7d31da1e558b4a7b21
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-01-28 23:14:02 -0200

    DATABASE CHANGE: Adiciona constraints 'NOT NULL' em colunas da tabela credit_cards.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_010510) do
+ActiveRecord::Schema.define(version: 2019_01_29_011003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 2019_01_29_010510) do
   end
 
   create_table "credit_cards", force: :cascade do |t|
-    t.string "cardid"
-    t.bigint "user_id"
+    t.string "cardid", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_credit_cards_on_user_id"
