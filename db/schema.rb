6822f1487deb31f522313b65f7a0db81a3a8514e
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-01-28 22:20:47 -0200

    DATABASE CHANGE: Adiciona constraints 'NOT NULL' em colunas da tabela ticket_statuses.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_001149) do
+ActiveRecord::Schema.define(version: 2019_01_29_001900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,7 +120,7 @@ ActiveRecord::Schema.define(version: 2019_01_29_001149) do
   end
 
   create_table "ticket_statuses", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
