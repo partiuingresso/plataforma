065898b440bc5b96dc760f0bf61779fa6e734111
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-01-28 22:10:30 -0200

    Fix update_order_items_not_null_constraint.

--- db/migrate/20190128235747_update_order_items_not_null_constraint.rb
+++ db/migrate/20190128235747_update_order_items_not_null_constraint.rb
@@ -9,11 +9,11 @@ class UpdateOrderItemsNotNullConstraint < ActiveRecord::Migration[5.2]
   end
 
   def down
-  	change_column_null :order_items, :unit_price, false
-  	change_column_null :order_items, :quantity, false
-  	change_column_null :order_items, :total_price, false
-  	change_column_null :order_items, :offer_id, false
-  	change_column_null :order_items, :order_id, false
+    change_column_null :order_items, :unit_price, true
+    change_column_null :order_items, :quantity, true
+    change_column_null :order_items, :total_price, true
+    change_column_null :order_items, :offer_id, true
+    change_column_null :order_items, :order_id, true
   end
 
 end
