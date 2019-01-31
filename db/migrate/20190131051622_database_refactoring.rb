c16c0e588c7fa01911ee992cf21a4f61c1fdba5f
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-01-31 04:17:41 -0200

    DATABASE CHANGE: Adiciona a constraint check ao rollback da coluna removida.

--- db/migrate/20190131051622_database_refactoring.rb
+++ db/migrate/20190131051622_database_refactoring.rb
@@ -7,7 +7,6 @@ class DatabaseRefactoring < ActiveRecord::Migration[5.2]
 
   	add_reference :ticket_tokens, :order_item
   	remove_reference :ticket_tokens, :ticket, null: false
-  	remove_column :order_items, :unit_price, :decimal, precision: 10, scale: 2, null: false
   	add_column :offers, :quantity, :integer, null: false
   	add_column :offers, :available_quantity, :integer, null: false
   	add_column :offers, :start_t, :datetime, null: false
@@ -15,6 +14,7 @@ class DatabaseRefactoring < ActiveRecord::Migration[5.2]
 
   	reversible do |dir|
   		dir.up do
+        remove_column :order_items, :unit_price, :decimal, precision: 10, scale: 2, null: false
   			execute <<-SQL
   				ALTER TABLE offers
   					ADD CONSTRAINT quantities_check
@@ -29,13 +29,18 @@ class DatabaseRefactoring < ActiveRecord::Migration[5.2]
   			SQL
   		end
   		dir.down do
+        add_column :order_items, :unit_price, :decimal, precision: 10, scale: 2, null: false
   			execute <<-SQL
   				ALTER TABLE offers
   					DROP CONSTRAINT quantities_check;
 
   				ALTER TABLE offers
   					DROP CONSTRAINT chronological_order_check;
-  			SQL
+
+                ALTER TABLE order_items
+                    ADD CONSTRAINT unit_price_check
+                        CHECK (unit_price >= 0);
+            SQL
   		end
   	end
   end
