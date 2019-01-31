b7160cbfee9fd583a36b7231db842f732080887d
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-01-31 04:21:23 -0200

    Atualiza o model por conta da retirada da tabela ticket.

--- app/models/ticket.rb
+++
@@ -1,5 +0,0 @@
-class Ticket < ApplicationRecord
-  belongs_to :offer
-  belongs_to :order_item
-  has_many :ticket_tokens
-end
--- app/models/ticket_token.rb
+++ app/models/ticket_token.rb
@@ -1,5 +1,4 @@
 class TicketToken < ApplicationRecord
-  belongs_to :ticket
   belongs_to :order
   belongs_to :ticket_status
   has_one :validation
