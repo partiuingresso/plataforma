84d982a6a4531d992d33631e5c160692a9fdd580
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-01-31 04:26:50 -0200

    Atualiza as associações dos models por conta das mudanças no banco de dados.

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,7 +1,7 @@
 class OrderItem < ApplicationRecord
   belongs_to :offer
   belongs_to :order
-  has_many :tickets
+  has_many :ticket_tokens
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validate :offer_present
--- app/models/ticket_token.rb
+++ app/models/ticket_token.rb
@@ -2,4 +2,5 @@ class TicketToken < ApplicationRecord
   belongs_to :order
   belongs_to :ticket_status
   has_one :validation
+  belongs_to :order_item
 end
