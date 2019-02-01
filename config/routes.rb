8d37955d71486d2ca3759e64778a34be7042e386
tcortezi <tvcortezi@gmail.com>
2019-01-31 22:25:42 -0200

    Devise views e config.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { 
+  :registrations => "users/registrations",
+  :sessions => "users/sessions",
+  :passwords => "users/passwords", 
+  :confirmations => "users/confirmations"
+  }
   devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new'
-    get 'sign_up', to: 'devise/registrations#new'
-    delete 'sign_out', to: 'devise/sessions#destroy'
+    get 'login', to: 'users/sessions#new'
+    get 'signup', to: 'users/registrations#new'
+    delete 'signout', to: 'users/sessions#destroy'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
