0ee97ac597de362850665dc53cb33d138fc078ba
tcortezi <tvcortezi@gmail.com>
2019-02-08 00:28:40 -0200

    Edit form + Footer

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -47,7 +47,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :age, :cpf, :gender])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :birthday, :cpf, :gender])
   end
 
   # The path used after sign up.
