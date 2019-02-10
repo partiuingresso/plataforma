f560aee3914149b541865d97942c75823e3aee4a
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-02-10 15:13:34 -0200

    DATABASE CHANGE: Adiciona a coluna image_url na tabela events.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_043945) do
+ActiveRecord::Schema.define(version: 2019_02_10_170641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2019_02_06_043945) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["event_venue_id"], name: "index_events_on_event_venue_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
