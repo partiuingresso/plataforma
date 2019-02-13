eadad6b8184b73cd01402902f455722c2fb677f3
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-02-12 22:13:33 -0200

    Adiciona relação de User com Company na classe User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :credit_cards
   has_many :orders
+  belongs_to :company
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
