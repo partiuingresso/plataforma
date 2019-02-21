aa57afe273d4032ae9872bbdf3050da4c8828efb
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-02-21 02:51:10 -0300

    Criação de um validate que não permite um Order estar relacionado com mais de um evento.

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,4 +4,15 @@ class OrderItem < ApplicationRecord
   has_many :ticket_tokens
   has_one :event, through: :offer
 
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validate :offer_cannot_be_from_other_event
+
+  private
+
+	def offer_cannot_be_from_other_event
+		if event.present? && order.order_items.length > 0 && event != order.event
+			errors.add(:event, "one order can't have order items related to different events")
+		end
+	end
+
 end
