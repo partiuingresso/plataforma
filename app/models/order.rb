84351e30f705bd19b7864843f2f5bd0d16b09131
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-02-21 02:53:05 -0300

    Gera o numero do pedido.

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,7 @@ class Order < ApplicationRecord
   has_many :offers, through: :order_items
 
   before_create :set_order_status
+  before_create :generate_order_number
   before_save :update_subtotal
 
   accepts_nested_attributes_for :order_items
@@ -27,13 +28,31 @@ class Order < ApplicationRecord
     order_items.collect { |oi| oi.valid? ? (oi.quantity) : 0 }.sum
   end
 
-private
-  def set_order_status
-    self.order_status_id = 1
-  end
+  private
 
-  def update_subtotal
-    self[:subtotal] = subtotal
-  end
+    def generate_order_number
+      now = DateTime.now
+      year = (now.year % 100).to_s
+      month = now.month.to_s.rjust(2, "0")
+      day = now.day.to_s.rjust(2, "0")
+
+      order_number = year + month + day
+
+      loop do
+        order_number += rand(10 ** 6).to_s.rjust(7, "0")
+        order_number = order_number.to_i
+        break unless Order.exists?(number: order_number)
+      end
+
+      self.number = order_number
+    end
+
+    def set_order_status
+      self.order_status_id = 1
+    end
+
+    def update_subtotal
+      self[:subtotal] = subtotal
+    end
 
 end
