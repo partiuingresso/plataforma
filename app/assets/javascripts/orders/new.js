f5cc0aecced5435eb3b8feedb2abe947464344de
tcortezi <tvcortezi@gmail.com>
2019-02-25 21:01:01 -0300

    Validação dos campos de form do checkout

--- app/assets/javascripts/orders/new.js
+++ app/assets/javascripts/orders/new.js
@@ -1,19 +1,21 @@
 //=require creditcard-warder/creditcard-warder.min
 
-
-var input = document.getElementById('card-number');
+var cardInput = document.getElementById('card-number');
 var imagesDiv = document.querySelector('.level-right.cards');
 var images = imagesDiv.querySelectorAll('img');
+var form = document.getElementsByTagName('form')[0];
+var formInputs = document.querySelectorAll('input');
 
-input.addEventListener('input', function(e) { 
+// Seleciona bandeira do cartão
+cardInput.addEventListener('input', function(e) {
   for(let img of images) {
     img.classList.remove("unselected");
   }
-  var number = input.value;
+  var number = cardInput.value;
   var card = CreditcardWarder(number);
   var brand = card.getBrand();
 
-  if(input.value) {
+  if(cardInput.value) {
     for(let img of images) {
       if(img.id != brand) {
         img.classList.add("unselected");
@@ -22,28 +24,69 @@ input.addEventListener('input', function(e) {
   }
 });
 
-var form = document.getElementsByTagName('form')[0];
-form.addEventListener('submit', function(event) {
-  var number = input.value;
+// Valida número do cartão
+function validateCC() {
+  var number = cardInput.value;
   var card = CreditcardWarder(number);
   var validate = card.validate();
   var brand = card.getBrand();
 
   if(!validate) {
-    event.preventDefault();
-    var help = document.getElementById('cardHelp');
-    help.classList.remove('is-hidden');
-    input.classList.add('is-danger');
-    input.scrollIntoView({block: "end", behavior: "smooth"});
+    cardInput.classList.add('is-danger');
+    cardInput.insertAdjacentHTML('afterend', '<span class="help checkout is-danger">Número de cartão inválido.</span>');
+    cardInput.scrollIntoView({block: "end", behavior: "smooth"});
+
+    return false
   }
   else if(brand == "other") {
-    event.preventDefault();
-    var help = document.getElementById('cardHelp');
-    help.classList.remove('is-hidden');
-    input.classList.add('is-danger');
-    input.scrollIntoView({block: "end", behavior: "smooth"});
+    cardInput.classList.add('is-danger');
+    cardInput.insertAdjacentHTML('afterend', '<span class="help checkout is-danger">Número de cartão inválido.</span>');
+    cardInput.scrollIntoView({block: "end", behavior: "smooth"});
 
+    return false
+  }
+
+  return true
+}
+
+// Valida os campos
+function validateInputs() {
+  var valid = true;
+  var complement = document.getElementById('payment_billing_address_complement');
+  for(let input of formInputs) {
+    if(!input.value.length && !(input == complement) && !(input.type == "hidden") && !(input == cardInput)) {
+      input.classList.add('is-danger');
+      input.insertAdjacentHTML('afterend', '<span class="help checkout is-danger">Este campo é obrigatório.</span>');
+      input.scrollIntoView({block: "end", behavior: "smooth"});
+      valid = false;
+    }
+  }
+
+  return valid
+}
+
+// Retira o help ao preencher
+function cleanInputs() {
+  for(let input of formInputs) {
+    input.addEventListener('focusin', function() {
+      var spanHelp = input.nextSibling;
+      input.classList.remove('is-danger');
+      spanHelp.classList.add('is-hidden');
+    });
+    input.addEventListener('focusout', function() {
+      if(!input.value.length && !(input == complement)) {
+        var spanHelp = input.nextSibling;
+        input.classList.add('is-danger');
+        spanHelp.classList.remove('is-hidden');
+      }
+    });
+  }
+}
+
+// Submit
+form.addEventListener('submit', function(e) {
+  if(!validateCC() || !validateInputs()) {
+    e.preventDefault();
+    cleanInputs();
   }
 });
-
-
