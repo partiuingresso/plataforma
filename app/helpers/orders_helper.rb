b113306ead411857cdadfba10630b8c3d3536ee5
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-02-27 01:25:08 -0300

    Ajusta a descrição nas opções de parcelamento.

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,6 +1,6 @@
 module OrdersHelper
 	def installment_info(count)
-		count.to_s + "x de R$" + price_with_interest(count)
+		count.to_s + "x de R$" + value_to_currency((@order.total * (1 + Wirecard::InterestTable[count])) / count)
 	end
 
 	def price_with_interest(count)
