054b064bd4ee26efae0f623cc87ea78969efdcac
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-03-01 23:38:36 -0300

    Alterações na atualização da conta bancária.

--- app/controllers/company_finances_controller.rb
+++ app/controllers/company_finances_controller.rb
@@ -15,14 +15,16 @@ class CompanyFinancesController < ApplicationController
 					flash[:notice] = "Conta bancária criada com sucesso."
 					redirect_to edit_company_finance_path(@company_finance)
 				else
-					@company_finance.destroy
-					render plain: bank_account.inspect
+					flash[:danger] = "Erro ao registrar conta bancária moip."
+					redirect_back(fallback_location: new_company_finance_path)
 				end
 			else
-				render plain: @company_finance.errors.messages
+				flash[:danger] = "Erro ao salvar conta bancária."
+				redirect_back(fallback_location: new_company_finance_path)
 			end
 		else
-			render plain: account.inspect
+			flash[:danger] = "Erro ao criar conta moip."
+			redirect_back(fallback_location: new_company_finance_path)
 		end
 	end
 
@@ -32,16 +34,18 @@ class CompanyFinancesController < ApplicationController
 
 	def update
 		@company_finance.assign_attributes(company_finance_params)
-		bank_account = Wirecard::bank_accounts(@company_finance).first
-		if Wirecard::update_bank_account @company_finance, bank_account.id
-			if @company_finance.save
+		if @company_finance.valid?
+			bank_account = Wirecard::bank_accounts(@company_finance).first
+			if Wirecard::update_bank_account @company_finance.reload, bank_account.id
+				@company_finance.save
 				flash[:notice] = "Conta bancária atualizada com sucesso."
 				redirect_to edit_company_finance_path(@company_finance)
 			else
-				redirect_back(fallback_location: edit_company_finance_path(@company_finance))
+				render plain: "Erro ao atualizar a conta bancária. Revise os campos enviados."
 			end
 		else
-			render plain: "Erro ao atualizar a conta bancária. Revise os campos enviados."
+			flash[:danger] = "Conta bancária inválida."
+			redirect_back(fallback_location: edit_company_finance_path(@company_finance))
 		end
 	end
 
