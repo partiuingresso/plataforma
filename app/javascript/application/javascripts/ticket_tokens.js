788c8e6f75c53ec3712062e66db321b4684349da
tcortezi <tvcortezi@gmail.com>
2019-03-20 21:53:33 -0300

    Ajusta a visualização do QR Code na página de ingressos

--- app/javascript/application/javascripts/ticket_tokens.js
+++ app/javascript/application/javascripts/ticket_tokens.js
@@ -2,11 +2,19 @@ show_tickets();
 
 function show_tickets() {
   let ticketBtn = document.querySelectorAll('.ticket');
+  let closeBtn = document.querySelectorAll('.closeBtn');
   for(let t of ticketBtn) {
     t.addEventListener('click', function() {
-      let modal = t.nextElementSibling;
-      modal.classList.add('is-active');
+      let show = t.nextElementSibling;
+      show.classList.add('op-1');
       document.documentElement.classList.add('is-clipped');
+    });
+  }
+  for(let c of closeBtn) {
+    c.addEventListener('click', function() {
+      let close = c.parentElement.parentElement.parentElement.parentElement;
+      close.classList.remove('op-1');
+      document.documentElement.classList.remove('is-clipped');
     });
   }
 }
