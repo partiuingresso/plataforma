3b0667cbbf7725376225bff1453b096f79acd765
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-03-23 22:28:07 -0300

    Utiliza nova library para escanear o qr code.

--- app/javascript/application/javascripts/validations.js
+++ app/javascript/application/javascripts/validations.js
@@ -1,59 +1,31 @@
 import Rails from 'rails-ujs';
 
-import QrScanner from 'qr-scanner';
-import QrScannerWorkerPath from '!!file-loader!../../../../node_modules/qr-scanner/qr-scanner-worker.min.js';
-QrScanner.WORKER_PATH = QrScannerWorkerPath;
+/*
+ *
+ * This module depends on Instascan library, which is included in new_validation.html 
+ * template using asset pipeline.
+ *
+ */
 
-var form = document.getElementById('new_validation');
-var ticketCodeInput = document.getElementById('ticket_token_code');
-
-var qrScanner;
-initScanner();
-
-var validated = false;
-var hold = false;
-
-var button = document.getElementById("open-camera");
-button.addEventListener('click', function() {
-	startScanner();
+var scanner = new Instascan.Scanner({
+	video: document.getElementById('qr-video'),
+	mirror: false,
+	backgroundScan: false
 });
 
-form.addEventListener('ajax:success', function(e) {
+scanner.addListener('scan', function(content) {
 	var resultElem = document.getElementById('validation-result');
-	resultElem.innerText = "Validação feita com sucesso!!!!!";
-	var [data, status, xhr] = e.detail
-	console.log('data: ', data);
-	console.log('status: ', status);
-	console.log('xhr: ', xhr);
-	validated = true;
-	hold = false;
+	resultElem.innerText = "Validação feita com sucesso: " + content + " -> " + Date().toString();
+	var input = document.getElementById('ticket_token_code');
+	input.value = content;
 });
 
-function initScanner() {
-	var videoElem = document.getElementById('qr-video');
-	qrScanner = new QrScanner(videoElem, result => qrCodeScanned(result));
-	qrScanner.setInversionMode("both");
-}
-
-function resetScanner() {
-	qrScanner.stop();
-	qrScanner._qrWorker.terminate();
-	initScanner();
-}
-
-function startScanner() {
-	qrScanner.start();
-}
-
-function qrCodeScanned(result) {
-	if(!(validated || hold)) {
-		hold = true;
-		var resultElem = document.getElementById('validation-result');
-		resultElem.innerText = "Validação feita com sucesso: " + result + " -> " + Date().toString();
-		ticketCodeInput.value = result;
-		resetScanner();
-		// Rails.fire(form, 'submit');
-		hold = false;
+Instascan.Camera.getCameras().then(function(cameras) {
+	if(cameras.length > 0) {
+		scanner.start(cameras[0]);
+	} else {
+		console.error('No cameras found');
 	}
-}
-
+}).catch(function(e){
+	console.error(e);
+});
