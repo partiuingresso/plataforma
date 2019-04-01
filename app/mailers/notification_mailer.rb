159931297a3a6c546b8e2f3e2b945fd82c12dc16
tcortezi <tvcortezi@gmail.com>
2019-04-01 18:07:34 -0300

    Adiciona template de email dos ingressos legacy

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -18,4 +18,12 @@ class NotificationMailer < ApplicationMailer
     attachments["#{@ticket_token.owner_name}-#{@ticket_token.event.name}-PartiuIngresso.pdf"] = VoucherPdfDiscreet.new(@ticket_token).render
     mail(to: @ticket_token.owner_email, subject: "Seu ingresso - #{@order.event.name}!")
   end
+
+  def legacy_tickets
+    @order = params[:order]
+    @order.ticket_tokens.each do |t|
+      attachments["#{t.owner_name}-#{t.event.name}-PartiuIngresso.pdf"] = VoucherPdf.new(t).render
+    end
+    mail(to: @order.user.email, subject: "Ingresso(s) - #{@order.event.name}!")
+  end
 end
