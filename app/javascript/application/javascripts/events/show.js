a99907e2cf0d060fba2f466cf0b16b4fc60b55fb
tcortezi <tvcortezi@gmail.com>
2019-04-01 01:53:10 -0300

    Adiciona bulma-carousel no js de show#events

--- app/javascript/application/javascripts/events/show.js
+++ app/javascript/application/javascripts/events/show.js
@@ -2,3 +2,4 @@ import "./cart";
 import "./colors.js.erb";
 import "./modal";
 import "../carousel";
+import * as bulmaCarousel from "../../../../../vendor/assets/javascript/bulma-carousel/bulma-carousel.js";
