78734279c7cda303475caca269549b6c501ce596
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-04-04 20:55:23 -0300

    Adiciona funcionalidade do webpack de substituição nos chunks.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,6 +6,6 @@ environment.loaders.prepend('erb', erb)
 environment.loaders.prepend('worker', worker)
 
 environment.config.delete('output.chunkFilename')
-environment.config.set('output.chunkFilename', '[name].bundle.js')
+environment.config.set('output.chunkFilename', '[name].[contenthash].bundle.js')
 
 module.exports = environment
