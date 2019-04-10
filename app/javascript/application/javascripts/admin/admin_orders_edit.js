460692320ec520cdd92759acbcf66942c5b0b09f
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-04-10 17:32:09 -0300

    Admin: permite editar nome e email do ingresso nos detalhes do pedido.

--- app/javascript/application/javascripts/admin/admin_orders_edit.js
+++ app/javascript/application/javascripts/admin/admin_orders_edit.js
@@ -1,44 +1,79 @@
 import Rails from 'rails-ujs';
-let editlinks = document.querySelectorAll(".icon.admin-orders.edit");
 
-for(let edit of editlinks) {
-  edit.addEventListener('click', function(e) {
-    let input = edit.parentElement.previousElementSibling.querySelector('.input.admin-orders');
-    let value = input.value;
-    let save = edit.nextElementSibling;
-    let form = input.form;
-    let done = false;
+var submit = false;
+init();
+
+function init() {
+  let orderRowList = document.querySelectorAll('tr.admin-orders');
+
+  for(let orderRow of orderRowList) {
+    let ownerNameEdit = orderRow.querySelector('.ticket-owner-name.edit');
+    let ownerNameSave = orderRow.querySelector('.ticket-owner-name.save');
+    let ownerNameInput = orderRow.querySelector('input.ticket-owner-name');
+    let ownerEmailEdit = orderRow.querySelector('.ticket-owner-email.edit');
+    let ownerEmailSave = orderRow.querySelector('.ticket-owner-email.save');
+    let ownerEmailInput = orderRow.querySelector('input.ticket-owner-email');
+
+    editInputOnClick(ownerNameInput, ownerNameEdit, ownerNameSave);
+    editInputOnClick(ownerEmailInput, ownerEmailEdit, ownerEmailSave);
+    submitOnClick(ownerNameSave, ownerNameInput.form);
+    submitOnClick(ownerEmailSave, ownerEmailInput.form);
+    beforeSubmit(ownerNameInput, ownerNameEdit, ownerNameSave);
+    beforeSubmit(ownerEmailInput, ownerEmailEdit, ownerEmailSave);
+    inputOnAjaxSuccess(ownerNameInput, ownerNameSave);
+    inputOnAjaxSuccess(ownerEmailInput, ownerEmailSave);
+  }
+}
+
+function editInputOnClick(input, editButton, saveButton) {
+  var currentValue;
+  editButton.addEventListener('click', function(e) {
+    currentValue = input.value;
+    submit = false;
+    input.disabled = false;
+    input.classList.remove('has-background-success');
     input.classList.remove('is-static');
     input.removeAttribute('readonly');
     input.focus();
-    edit.classList.add('is-hidden');
-    save.classList.remove('is-hidden');
-    save.addEventListener('click', function(e) {
-      console.log("olar");
-      if(done) {
-        preventDefault(e);
-      } else {
-        Rails.fire(form, 'submit', input);
-      }
-    });
-    form.addEventListener('ajax:success', function(e) {
-      save.classList.add('has-text-success');
-      input.classList.add('has-background-success');
-      input.classList.add('is-static');
-      input.setAttribute('readonly','');
-      const done = true;
-      console.log(done);
-    });
-    input.addEventListener('focusout', function clear(e) {
-      if(!done) {
-        console.log("focusout");
-        console.log(done);
-        input.value = value;
-        input.classList.add('is-static');
-        input.setAttribute('readonly','');
-        edit.classList.remove('is-hidden');
-        save.classList.add('is-hidden');
-      }
-    });
+    editButton.classList.add('is-hidden');
+    saveButton.classList.remove('is-hidden');
+  });
+
+  input.addEventListener('focusout', function(e) {
+    if(submit || e.relatedTarget === saveButton) {
+      return;
+    }
+    input.value = currentValue;
+    input.classList.add('is-static');
+    input.setAttribute('readonly','');
+    editButton.classList.remove('is-hidden');
+    saveButton.classList.add('is-hidden');
+  });
+}
+
+function beforeSubmit(input, editButton, saveButton) {
+  let form = input.form;
+  form.addEventListener('submit', function(e) {
+    submit = true;
+    input.classList.add('is-static');
+    input.setAttribute('readonly','');
+    editButton.classList.remove('is-hidden');
+    saveButton.classList.add('is-hidden');
+  });
+}
+
+function submitOnClick(button, form) {
+  button.addEventListener('click', function(e) {
+    Rails.fire(form, 'submit');
+  });
+}
+
+function inputOnAjaxSuccess(input, saveButton) {
+  let form = input.form;
+  form.addEventListener('ajax:success', function(e) {
+    input.classList.add('has-background-success');
+    input.classList.add('is-static');
+    input.setAttribute('readonly','');
+    input.disabled = true;
   });
 }
