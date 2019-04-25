c6bfb41f71f410fbe6e12f0c3570af9ea82568cd
tcortezi <tvcortezi@gmail.com>
2019-04-24 22:05:05 -0300

    Adiciona grafico de ingressos vendidos no dashboard de produtor

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -11,6 +11,13 @@ class ChartsController < ApplicationController
     render json: @orders.approved.group_by_day(:created_at, last: 28, format: "%d %b").sum('subtotal_cents / 100')
   end
 
+  def sales_tickets
+    @events = Event.where(company_id: current_user.company_id)
+    @orders = Order.where(event_id: @events)
+    @tickets = OrderItem.where(order_id: @orders.approved)
+    render json: @tickets.group_by_day(:created_at, last: 28, format: "%d %b").sum('quantity')
+  end
+
   def report_count
     offer = params[:offer]
     if offer.present?
