8a6a7598475f137ec134e49aea685aec0c70eeb2
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-05-02 19:26:48 -0300

    confirmação de recebimento de ingressos: substitui o cache para file cache.

--- config/initializers/legacy_confirmations_cache.rb
+++ config/initializers/legacy_confirmations_cache.rb
@@ -1,9 +1,11 @@
-confirmation_hash = {}
+$confirmation_cache = ActiveSupport::Cache::FileStore.new Rails.root.dirname.join("legacy_confirmation_cache")
 
-legacy_orders = Order.where(user_id: 3, status: :approved)
-legacy_orders.each do |order|
-	confirmation_hash[order.number] = false
+unless $confirmation_cache.exist?("confirmations")
+	confirmation_hash = {}
+
+	legacy_orders = Order.where(user_id: 3, status: :approved)
+	legacy_orders.each do |order|
+		confirmation_hash[order.number] = false
+	end
+	$confirmation_cache.write("confirmations", confirmation_hash)
 end
-
-$confirmation_cache = ActiveSupport::Cache::MemoryStore.new
-$confirmation_cache.write("confirmations", confirmation_hash)
