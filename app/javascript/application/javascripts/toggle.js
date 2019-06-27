55d6024c4bff1e6f01fc87afa8bda92b385072e9
tcortezi <tvcortezi@gmail.com>
2019-06-27 19:34:36 -0300

    Ajusta carrinho na pagina de venda e ads

--- app/javascript/application/javascripts/toggle.js
+++ app/javascript/application/javascripts/toggle.js
@@ -3,15 +3,19 @@ export function toggle() {
   let dismiss = document.querySelectorAll('.dismiss');
   for(let t of toggles) {
     t.addEventListener("click", function() {
-      var content = t.nextElementSibling;
+      var content = t.parentNode.parentNode.parentNode.childNodes[3];
+      var dismiss = t.nextElementSibling;
       content.classList.remove('is-hidden');
       t.classList.add('is-hidden');
+      dismiss.classList.remove('is-hidden');
     });
   }
   for(let d of dismiss) {
     d.addEventListener("click", function() {
       var toggle = d.previousElementSibling;
+      var content = d.parentNode.parentNode.parentNode.childNodes[3];
       toggle.classList.remove('is-hidden');
+      content.classList.add('is-hidden');
       d.classList.add('is-hidden');
     });
   }
