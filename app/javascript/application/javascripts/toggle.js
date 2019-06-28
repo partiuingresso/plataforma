f90581b65ddf017210a11c219056f8252414b962
tcortezi <tvcortezi@gmail.com>
2019-06-28 00:43:21 -0300

    Adiciona funcoes diferentes para o toggle de informações de FAQ e Cart nas paginas de evento

--- app/javascript/application/javascripts/toggle.js
+++ app/javascript/application/javascripts/toggle.js
@@ -3,6 +3,25 @@ export function toggle() {
   let dismiss = document.querySelectorAll('.dismiss');
   for(let t of toggles) {
     t.addEventListener("click", function() {
+      var content = t.nextElementSibling;
+      content.classList.remove('is-hidden');
+      t.classList.add('is-hidden');
+    });
+  }
+  for(let d of dismiss) {
+    d.addEventListener("click", function() {
+      var toggle = d.previousElementSibling;
+      toggle.classList.remove('is-hidden');
+      d.classList.add('is-hidden');
+    });
+  }
+}
+
+export function toggleCart() {
+  let toggles = document.querySelectorAll('.toggleCart');
+  let dismiss = document.querySelectorAll('.dismissCart');
+  for(let t of toggles) {
+    t.addEventListener("click", function() {
       var content = t.parentNode.parentNode.parentNode.childNodes[3];
       var dismiss = t.nextElementSibling;
       content.classList.remove('is-hidden');
