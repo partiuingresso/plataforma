38c2ff1b5d09d696e9b81f7a0374d62d515e14a3
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-07-17 12:41:10 -0300

    Instalação do vue.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
 const erb =  require('./loaders/erb')
 const worker = require('./loaders/worker')
@@ -8,4 +10,6 @@ environment.loaders.prepend('worker', worker)
 environment.config.delete('output.chunkFilename')
 environment.config.set('output.chunkFilename', '[name].[contenthash].bundle.js')
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
