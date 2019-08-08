6c56c8f512dc6ec9addeefbe700862219ae98c99
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-08-08 15:20:21 -0300

    Remoção completa do Asset Pipeline.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,14 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,6 +29,10 @@ module Plataforma
     config.eager_load_paths << Rails.root.join('lib')
     config.eager_load_paths << Rails.root.join('abilities')
     config.eager_load_paths << Rails.root.join('serializers')
+
+    config.generators do |g|
+        g.assets = nil
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
