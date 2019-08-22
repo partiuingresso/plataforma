58ee58d24c16f6b2ac98da8c1d8983ecab208241
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-08-22 18:30:34 -0300

    Tentativa de consertar o deploy: adiciona o modulo action cable.

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require 'active_record/railtie'
 require 'active_storage/engine'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
+require "action_cable/railtie"
 require 'action_view/railtie'
 require 'rails/test_unit/railtie'
 
