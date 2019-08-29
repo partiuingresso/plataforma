2997460c2942d79770246564af2ef14e76b4545e
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-08-29 20:29:17 -0300

    Conserta o js da página de evento

--- app/frontend/src/events/ads.js
+++ app/frontend/src/events/ads.js
@@ -1,4 +1,11 @@
 import './colors-ads'
 import '../carousel.js.erb'
-import './cart'
-import './modal-ads'
+import cart from './cart'
+import modal from './modal-ads'
+
+export default {
+	init() {
+		cart.init()
+		modal.init()
+	}
+}
