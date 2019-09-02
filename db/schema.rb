bed5fa11a246955fa5ec7c7a6fb6a8ab906e8099
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-09-02 18:35:33 -0300

    Remove null constraint de colunas temporárias na tabela sellers.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_31_053641) do
+ActiveRecord::Schema.define(version: 2019_09_02_213136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
