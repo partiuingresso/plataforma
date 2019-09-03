0f59b3fef79a6c6bda540bf4e1130ae1ac168e00
tcortezi <tvcortezi@gmail.com>
2019-09-03 04:32:09 -0300

    Header principal desktop

--- app/frontend/src/custom.js
+++ app/frontend/src/custom.js
@@ -1,21 +1,26 @@
-// Navbar Burger
+var dropdownLink = document.querySelector('.navDroplink')
+var dropdown = document.querySelector('.navDropdown')
 
-document.addEventListener('DOMContentLoaded', () => {
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  if ($navbarBurgers.length > 0) {
-
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
+var toggleDropdown = function(e) {
+  if(dropdown.classList.contains('show')) {
+    dropdown.classList.remove('show')
+  } else {
+    dropdown.classList.add('show')
   }
+}
 
-});
+var hideDropdown = function(e) {
+  if (e.target != dropdownLink && !dropdownLink.contains(e.target) && !dropdown.contains(e.target)) {
+    if (dropdown.classList.contains('show')) {
+      dropdown.classList.remove('show')
+    }
+    else {
+      return
+    }
+  }
+}
+
+if(dropdownLink) {
+  dropdownLink.addEventListener('click', toggleDropdown)
+  document.addEventListener('click', hideDropdown)
+}
