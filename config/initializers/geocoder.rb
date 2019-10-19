7bc710292939d29e4f1202c98ed2a9954016e4a5
tcortezi <tvcortezi@gmail.com>
2019-10-18 22:06:08 -0300

    Geocoder: ajusta configuração e altera request.location da home

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,8 +1,8 @@
 Geocoder.configure(
   # Geocoding options
-  # timeout: 3,                 # geocoding service timeout (secs)
+  timeout: 3,                 # geocoding service timeout (secs)
   lookup: :google,              # name of geocoding service (symbol)
-  # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
+  ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
@@ -17,6 +17,6 @@ Geocoder.configure(
   # always_raise: [],
 
   # Calculation options
-  # units: :mi,                 # :km for kilometers or :mi for miles
-  # distances: :linear          # :spherical or :linear
+  units: :km,                 # :km for kilometers or :mi for miles
+  distances: :linear          # :spherical or :linear
 )
