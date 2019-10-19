837d3eefc34273e11ed70c5894a52623f2cc2ce7
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2019-10-18 21:37:24 -0300

    criação de conta transparente wirecard usa o celular de person

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,8 +5,6 @@ class Company < ApplicationRecord
 	validates :name, presence: true
 	validates :business_name, presence: true
 	validates :document_number, presence: true
-	validates :phone_area_code, presence: true
-	validates :phone_number, presence: true
 
 	accepts_nested_attributes_for :address
 end
