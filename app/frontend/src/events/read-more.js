2bdf4c36a72adb055d6656386d6ef872e4b515fa
tcortezi <tvcortezi@gmail.com>
2019-10-22 04:34:09 -0300

    Bugfix: corrige erro do 'leia mais', a altura estava sendo coletada antes da pagina carregar completamente

--- app/frontend/src/events/read-more.js
+++ app/frontend/src/events/read-more.js
@@ -2,13 +2,15 @@ export default {
   init() {
     var text = document.querySelector('.text')
     var readMore = text.querySelector('.readMore')
-    if(text.offsetHeight >= 720) {
-      text.addEventListener('click', function(e) {
+    window.addEventListener("load", function(event) {
+      if(text.offsetHeight >= 720) {
+        text.addEventListener('click', function(e) {
+          readMore.classList.add('is-hidden')
+          text.classList.add('more')
+        })
+      } else {
         readMore.classList.add('is-hidden')
-        text.classList.add('more')
-      })
-    } else {
-      readMore.classList.add('is-hidden')
-    }
+      }
+    })
   }
 }
