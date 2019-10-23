d122fcea062b5de5775057508793b51a34609220
tcortezi <tvcortezi@gmail.com>
2019-10-23 00:19:42 -0300

    Bugfix: não permite que números do rgb de colors.js tenham decimais para evitar bug em alguns navegadores

--- app/frontend/src/events/colors.js
+++ app/frontend/src/events/colors.js
@@ -2,15 +2,29 @@ import * as Vibrant from 'node-vibrant'
 
 let v = new Vibrant(imgurl)
 v.getPalette((err, palette) => {
-    let darkvibrant = palette.DarkVibrant.getRgb()
-    let vibrant = palette.Vibrant.getRgb()
-    let muted = palette.Muted.getRgb()
-    let darkmuted = palette.DarkMuted.getRgb()
-    let lightvibrant = palette.LightVibrant.getRgb()
-    let lightmuted = palette.LightMuted.getRgb()
+	let vibrant = palette.Vibrant.getRgb()
+	let lightvibrant = palette.LightVibrant.getRgb()
+	let darkvibrant = palette.DarkVibrant.getRgb()
+	let muted = palette.Muted.getRgb()
+	let lightmuted = palette.LightMuted.getRgb()
+	let darkmuted = palette.DarkMuted.getRgb()
 
-    var style = document.createElement('style')
-    style.type = 'text/css'
-    style.innerHTML = ':root { --darkvibrant: '+darkvibrant+'; --vibrant: '+vibrant+'; --muted: '+muted+'; --darkmuted: '+darkmuted+'; --lightvibrant: '+lightvibrant+'; --lightmuted: '+lightmuted+'; }'
-    document.getElementsByTagName('head')[0].appendChild(style)
+	function decToInt(rgb) {
+		rgb[0] = parseInt(rgb[0])
+		rgb[1] = parseInt(rgb[1])
+		rgb[2] = parseInt(rgb[2])
+	}
+
+	decToInt(vibrant)
+	decToInt(lightvibrant)
+	decToInt(darkvibrant)
+	decToInt(muted)
+	decToInt(lightmuted)
+	decToInt(darkmuted)
+
+
+  let style = document.createElement('style')
+  style.type = 'text/css'
+  style.innerHTML = ':root { --darkvibrant: '+darkvibrant+'; --vibrant: '+vibrant+'; --muted: '+muted+'; --darkmuted: '+darkmuted+'; --lightvibrant: '+lightvibrant+'; --lightmuted: '+lightmuted+'; }'
+  document.getElementsByTagName('head')[0].appendChild(style)
 })
